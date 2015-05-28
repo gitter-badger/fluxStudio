c86506a60648474bdac4b10fe19a6bc22c2fb16b
Gaetano Checinski <Gaetano.Checinski@gmail.com>
2015-05-28 08:55:58 +0200

    bugfixing decorators

--- client.js
+++ client.js
@@ -5,14 +5,14 @@ import React  from 'react/addons'
 import use    from './decorators/decorator'
 
 var app={};
+window.app=app;
+
 app.event = new emmett();
 
 app.state = baobab({
   msg:'hello',
-  inc:1,
-  app:app
+  inc:1
 });
-
 
 
 app.registerAction = function(name,func){
@@ -26,27 +26,30 @@ app.registerAction('inc',function(state){
 });
 
 app.inject=use.rootInjector(app.state);
+
+
 app.action=((name,data)=>
   app.event.emit.bind(app.event,name,{state:app.state,app:app,data:data})
 );
 
-
-@use.pure
-@use.action
-@use.cursors({'msg':['msg'],'inc':['inc']})
+@use.cursors({'msg':['msg']})
+@use.cursors({'inc':['inc']})
+@use.getContext(['app'])
 class App2 extends React.Component {
   render(){
-    return (<div> <h1>{this.props.msg}</h1> <button onClick={ this.props.action('inc') }>{this.props.inc}</button> </div>);
+    return (<div> <h1>{this.props.msg}</h1> <button onClick={ this.props.app.action('inc') }>{this.props.inc}</button> </div>);
   }
 };
 
+
 @app.inject
+@use.pure
+@use.contextInjector( {app:app} )
 class App extends React.Component {
   render(){
     return <App2/>
   }
 };
-
 
 
 setTimeout(()=>{ app.state.set('msg','world'); },1000);
--- decorators/decorator.js
+++ decorators/decorator.js
@@ -5,13 +5,59 @@ var root=baobab.root;
 var branch=baobab.branch;
 var dec={};
 
-dec.getBaobabContext = function getBaobabContext(comp){
- return extend({},comp,{contextTypes:{
-    tree: PropTypes.baobab,
-    cursors: PropTypes.cursors,
-    facets: PropTypes.facets
-  }});
+dec.getContext = function(context){
+  var obj={};
+
+  if(context.constructor.name == 'Array'){
+    context.forEach(function(key){
+      obj[key] = React.PropTypes.any; 
+    }); 
+  }else{
+    obj=context;
+  }
+
+  return function(comp){
+    return (class ComposedClass {
+      static contextTypes = extend(obj,comp.contextTypes) 
+      render(){
+        return React.createElement(comp,extend({},this.props,this.context));
+      }
+    });
+  };
 };
+
+
+
+dec.contextInjector = function injectContext(context){
+  let obj={};
+  for(let k in context) obj[k] = React.PropTypes.any
+  return function(comp){
+    return (class ComposedClass extends comp { 
+      static childContextTypes = extend(obj,comp.childContextTypes) 
+      getChildContext(){
+        return extend( ((super.getChildContext&&super.getChildContext())||{}) , context);
+      }
+    });
+  };
+};
+
+dec.mixins = function mixins(mix){
+  return function(comp){
+    return (class ComposedClass extends comp { 
+      constructor(props,context){
+        super(props,context);
+
+        mix.forEach((m)=>{
+          for(let k in m){
+            this[k]=m[k];
+          }
+        });
+      }
+    });
+  };
+};
+
+
 
 dec.rootInjector=function rootInjector(state){
   return function(comp){
@@ -25,23 +71,16 @@ dec.branch=function branchInjector(obj){
   };
 };
 
-dec.pure=function pure(comp){
-  comp.mixins=comp.mixins||[];
-  comp.mixins.push(React.pureRenderMixin);
-  return comp;  
-};
 
-dec.linkedState=function linkedState(comp){
-  comp.mixins=comp.mixins||[];
-  comp.mixins.push(React.linkedStateMixin);
-  return comp;  
-};
+dec.pure=dec.mixins([React.addons.PureRenderMixin]);
+dec.linkedState=dec.mixins([React.addons.PureRenderMixin]);
 
 dec.commitAfter=function commitAfter(action){
-  var that=this;
+  let that=this;
   return function(state){
-    setTimeout(state.commit.bind(state));
-    return action.apply(that,arguments);
+    let res = action.apply(that,arguments);
+    state.commit();
+    return res;
   };
 };
 
@@ -53,9 +92,7 @@ dec.facets=function facets(obj){
   return dec.facets({facets:obj});
 };
 
-dec.app=dec.cursors({app:['app']});
-
-dec.action=dec.cursors({action:['app','action']});
+dec.app=dec.getContext(['app']);
 
 module.exports=dec;
 
